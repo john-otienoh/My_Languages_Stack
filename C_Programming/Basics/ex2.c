0ba0f19f4cbd0c568134da88cf70f3f7b6babd46
john-otienoh <johnteclaire@gmail.com>
2025-03-31 12:44:06 +0300

    C basics (Constants, variables, operators)

---
+++ C_Programming/Basics/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float CONSTANT = 3.14; //Constant
+    int variable = 10; //Variable
+    float expression = (2 + 3) * 10;
+}
+
+int add(int x, int y)
+{
+    int result;
+    result = x + y;
+    return (result);
+}
--- C_Programming/hello.c
+++ C_Programming/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
-void main(){
-    printf("Hello World")
+/*
+ * main: Program Entry
+*/
+int main(void)
+{
+    printf("Hello World....My first C Program!\n");
+    return (0);
 }
