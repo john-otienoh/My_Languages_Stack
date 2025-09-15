3982f81516a8c5730806510f192c2377ebe05057
john-otienoh <johnteclaire@gmail.com>
2025-09-15 15:16:34 +0300

    Naming Conventions in Go

---
+++ Go_Programming/golang_in_24_hours/naming_conventions/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Foo string = "constant string"
+
+func main() {
+	fmt.Println(Foo)
+	a_string := "hello"
+	fmt.Println(a_string)
+	fmt.Println("Hello World")
+
+	// Using golint, gofmt, godoc
+}
